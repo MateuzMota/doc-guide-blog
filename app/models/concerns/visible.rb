11f771ed45fce15e932ba41b9476048a69694920
Mateus Mota <m4t3u5.m0t4@gmail.com>
2024-08-08 18:28:11 -0300

    feat: add counter to public articles

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,12 +1,20 @@
 module Visible
-  extend ActiveSupport::Concern
+  extend ActiveSupport::Concern # included, class_methods
 
   VALID_STATUSES = ['public', 'private', 'archived']
 
+  # all code inside 'included' are executed in the class that includes this module
   included do
-    validates :status, inclusion: { in VALID_STATUSES }
+    validates :status, inclusion: { in: VALID_STATUSES }
   end
-  
+
+  # all methods inside 'class_methods' are defined in the class that includes this module
+  class_methods do
+    def public_count
+      where(status: 'public').count
+    end
+  end
+
   def archived?
     status == 'archived'
   end
