6e5e042db6c96dfe89d23cfb7a6a1d8ce91670cf
Mateus Mota <m4t3u5.m0t4@gmail.com>
2024-08-08 17:58:36 -0300

    feat: verify archived articles to prevent inappropriate rendering

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,4 +7,8 @@ class Article < ApplicationRecord
   VALID_STATUSES = ['public', 'private', 'archived']
 
   validates :status, inclusion: { in: VALID_STATUSES }
+
+  def archived?
+    status == 'archived'
+  end
 end
